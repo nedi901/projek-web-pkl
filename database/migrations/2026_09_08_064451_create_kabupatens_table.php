536ab7e4dcdebe736a59138f5b6a6d5117f49f48
Nama Kamu <email-kamu@gmail.com>
2026-09-08 14:22:10 +0700

    menyimpan hasil migration nya

--- database/migrations/2026_09_08_064451_create_kabupatens_table.php
+++ database/migrations/2026_09_08_064451_create_kabupatens_table.php
@@ -6,20 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('kabupatens', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('provinsi_id')->constrained('provinsis');
+            $table->string('nama_kabupaten');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('kabupatens');
